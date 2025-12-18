60232793f5df865b2effeff2a6a062949c26176c
arriatea <marcoas.arriate@gmail.com>
2025-12-18 00:29:36 -0300

    Deploy Bug fix 2

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,16 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Home } from "./pages/Home";
-import { NotFound } from "./pages/NotFound";
+import { Routes, Route } from "react-router-dom"
+import { Home } from "./pages/Home"
+import { NotFound } from "./pages/NotFound"
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-  );
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  )
 }
 
-export default App;
+export default App
